4c3b3df55714e3db676ce16c3080b123420afaed
Gaurav Gupta <gaurav33dev@gmail.com>
2025-05-11 01:19:10 +0530

    minor-changes

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,5 +1,5 @@
 export const USER_API_END_POINT="https://jobquest-my-academic-projects.up.railway.app/api/v1/user";
-export const JOB_API_END_POINT="https://jobquest-my-academic-projects.up.railway.app/v1/job";
+export const JOB_API_END_POINT="https://jobquest-my-academic-projects.up.railway.app/api/v1/job";
 export const APPLICATION_API_END_POINT="https://jobquest-my-academic-projects.up.railway.app/api/v1/application";
 export const COMPANY_API_END_POINT="https://jobquest-my-academic-projects.up.railway.app/api/v1/company";
 
@@ -8,3 +8,4 @@ export const COMPANY_API_END_POINT="https://jobquest-my-academic-projects.up.rai
 // export const JOB_API_END_POINT="http://localhost:8000/api/v1/job";
 // export const APPLICATION_API_END_POINT="http://localhost:8000/api/v1/application";
 // export const COMPANY_API_END_POINT="http://localhost:8000/api/v1/company";
+
