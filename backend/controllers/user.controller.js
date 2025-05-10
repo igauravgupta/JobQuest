ea6e41a59ebf9d747e460b44cc39e543ea471635
Gaurav Gupta <gaurav33dev@gmail.com>
2025-05-11 01:07:06 +0530

    sameSite:None-for-deployment

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -95,7 +95,7 @@ export const login = async (req, res) => {
 
         // secure : true for production ( https ) but in local ( http )
         return res.status(200)
-        .cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true, sameSite: 'strict' , secure:process.env.NODE_ENV === "production" })
+        .cookie("token", token, { maxAge: 1 * 24 * 60 * 60 * 1000, httpOnly: true,  sameSite: 'None' , secure:process.env.NODE_ENV === "production" })
         .json({
             message: `Welcome back ${user.fullname}`,
             user,
